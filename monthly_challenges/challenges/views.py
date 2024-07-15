cdce3e3c8d78327eefc234e5cbc772537e90a341
Jbasilio55 <j.l.basilio55@gmail.com>
2024-07-15 18:50:16 -0400

    add new challenges app and made routes for Jan and Feb

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def january(request):
+    return HttpResponse("New Years Resolution, Start working out and get that body on point.")
+
+def february(request):
+    return HttpResponse("Work on algorithms and become a pro!!")
